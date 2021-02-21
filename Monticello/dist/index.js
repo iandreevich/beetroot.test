688003662d9058395f280f6e2f0e8485fe459bb7
iandreevich <iadnreevich228@gmail.com>
2021-02-21 17:16:53 +0200

    naxui

--- Monticello/dist/index.js
+++ Monticello/dist/index.js
@@ -7,3 +7,11 @@ $(".slider").slick({
   autoplaySpeed: 3000,
   pauseOnHover: false,
 });
+$(".news__slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  // autoplay: true,
+  // autoplaySpeed: 2000,
+  prevArrow: '<img src="img/arrow_left.png" alt="prev" class="left">',
+  nextArrow: '<img src="img/arrow_right.png" alt="next" class="right">',
+});
