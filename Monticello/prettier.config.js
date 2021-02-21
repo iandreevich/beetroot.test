55cc7c1b2cc64f7363b65dc10ddbc870df23d9fe
iandreevich <iadnreevich228@gmail.com>
2021-02-21 22:13:16 +0200

    ppp

---
+++ Monticello/prettier.config.js
@@ -0,0 +1,15 @@
+const config = {
+    singleQuote: true,
+    useTabs: false,
+    trailingComma: 'none',
+    tabWidth: 4,
+    semi: true,
+    printWidth: 100,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    bracketSpacing: true,
+    requirePragma: false,
+    insertPragma: false
+};
+
+module.exports = config;
